78a0f20b661b848a4691847bdfdba7fc52b02f10
あなたの名前 <you@example.com>
2026-08-01 14:23:34 +0900

    Set headless=True for GitHub Actions

--- src/SapporoReserve.py
+++ src/SapporoReserve.py
@@ -114,7 +114,7 @@ def main():
 
     with sync_playwright() as p:
 
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         login(page)
